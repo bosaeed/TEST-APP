2ff96889919502cb8955ea3f52319c7f6a802462
abdullah jaber <bin_ali_jaber@hotmail.com>
2020-08-18 23:02:05 +0300

    start

---
+++ TEST APP/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace TEST_APP.Models
+{
+    public class Note
+    {
+        [PrimaryKey , AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ TEST APP/NoteEntryPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST_APP.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TEST_APP
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntry : ContentPage
+    {
+        public NoteEntry()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
